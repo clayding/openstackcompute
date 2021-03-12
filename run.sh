4c2ce2992d35aa9ee40f40e0ab4ffc53d05cbbc0
Shikun Ding <shikun.ding@intel.com>
2021-03-12 20:13:21 +0800

    Made services running on background
    Signed-off-by: Shikun Ding <shikun.ding@intel.com>

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --privileged=true --network host --rm -h OpenstackCompute -it clayding/openstackcompute:v1.0.1 /bin/bash
+docker run --privileged=true --network host --rm -h OpenstackCompute -it clayding/openstackcompute:v1.0.2 /usr/sbin/init
