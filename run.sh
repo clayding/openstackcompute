311c39d7b8d98fba8df9ab1e2ae8b9fe3a88607c
Ding, Shikun <shikun.ding@intel.com>
2021-03-16 12:58:07 +0800

    Added options and descriptions in run.sh
    Signed-off-by: Ding, Shikun <shikun.ding@intel.com>

--- run.sh
+++ run.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
-if [ $# != 2 ];
+if [ $# != 5 ];
 then
 echo "Usage:"
-echo "	bash run.sh [host name] [image name]"
+echo "	bash run.sh [host name] [image name] [controller ip] [compute ip] [provider interface]"
+
+echo "Options:"
+echo "	host name:          Set the hostname for being created container"
+echo "	image name:         Specify image's name from which the container is created"
+echo "	controller ip:      IP address of controller node on which the keystone, rabbitMQ are running"
+echo "	compute ip:         Management IP address on compute node (same subnet with controller ip)"
+echo "	provider interface: Network interface for provider network"
+
+
 echo "Example:"
-echo "	bash run.sh OpenstackCompute clayding/openstackcompute:v1.0.0"
+echo "	bash run.sh OpenstackCompute clayding/openstackcompute:v1.0.0 192.168.100.110 192.168.100.12 eth0"
+
+exit 0
 fi
 
 host_name=$1
 imagename=$2
+controlip=$3
+computeip=$4
+provideif=$5
 
-docker run --privileged=true --network host --rm -h ${host_name} -it ${imagename} /usr/sbin/init
+docker run --privileged=true --network host --rm -h ${host_name} -it ${imagename} \
+	-e CONTROLLER_IP=$controlip -e COMPUTE_IP=$computeip -e PROVIDER_INTERFACE=$provideif \
+	/usr/sbin/init
